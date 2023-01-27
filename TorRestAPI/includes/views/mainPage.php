02b345bff8a530327bd1893616639372acf0a394
Gustavo Martins <gustavo_mts2005@hotmail.com>
2023-01-27 11:27:10 -0300

    Adding search input

--- TorRestAPI/includes/views/mainPage.php
+++ TorRestAPI/includes/views/mainPage.php
@@ -1,6 +1,7 @@
 <?php $currentPage = (isset($_GET['page']) && filter_var($_GET['page'], FILTER_VALIDATE_INT)) ? $_GET['page'] : 1;
-      $pagination = new Pagination(count($allIPs));
+      $pagination = new Pagination(count($allIPs), $limit ?? 54);
       $allIPs = $pagination->getResults($allIPs, $currentPage); ?>
+<!-- setting the pagination to all pages that are using this file -->
 
 <div class="container bg-purple p-4 rounded-bottom">
     <?php if(isset($_SESSION['message'])) { ?>
@@ -9,25 +10,70 @@
     <?php }
     ?>
 
-    <form method="POST" action="../ocultar-ip/index.php" class="mb-3 row">
-        <div class="col-md-10">
-            <input type="text" placeholder="Ex.: 104.53.221.159" class="form-control" name="AddressIP" required>
-        </div>
-        <div class="col-md-2"><input type="submit" class="btn btn-primary rounded" value="Ocultar IP"></div>
+    <form method="<?php echo (TITLE == 'Tor REST API') ? 'GET' : 'POST' ?>"
+    action="<?php echo TITLE == 'Tor REST API' ? null : firstEndpoint ?>" 
+    class="mb-3 input-group form-row">
+        <input type="text" placeholder="Ex.: 104.53.221.159" class="form-control w-75" name="AddressIP"
+        <?php echo TITLE == 'Tor REST API' ? "value='$search' oninput='this.form.submit()'" : "required" ?>>
+        <?php if(TITLE == 'Tor REST API') { ?> <!-- you are at the index page -->   
+                <label class="input-group-text">IPs por página</label>
+                <input type="number" name="limit" value="<?php echo $limit ?>" class="form-control col-auto" min="1">
+
+                <button class="btn bg-dark text-light input-group-addon" type="submit"><i class="bi bi-search"></i></button>
+        <?php } else { ?>
+                <input type="submit" class="btn btn-warning input-group-addon" value="<?php
+                    if(firstEndpoint == '../ips-desocultos/desocultarIP.php')
+                        echo "Desocultar IP";
+                    else
+                        echo "Ocultar IP";
+                ?>">
+        <?php } ?>
     </form>
 
-    <div class="d-flex justify-content-center">
-        <?php echo $pagination->getButtonsSection($currentPage, 16); ?>
-    </div>
+    <!-- current page button is always at the middle, having 2 buttons to the left and 2 to the right -->
+    <?php echo $pagination->getButtonsSection($currentPage, 5); ?>
     
+    <div style="float: right" class="mx-4 mb-3">
+        <a href="<?php echo secndEndpoint ?>">
+            <button class="btn btn-primary">
+            <?php 
+                if(secndEndpoint == '../')
+                    echo 'Todos os IPs';
+                else
+                    echo 'IPs ocultos';
+            ?>
+            </button>
+        </a>
+
+        <a href="<?php echo thirdEndpoint ?>">
+            <button class="btn btn-primary">
+                <?php
+                    if(thirdEndpoint == '../ips-ocultos')
+                        echo 'IPs ocultos';
+                    else
+                        echo 'IPs não ocultos';
+                ?>
+            </button>
+        </a>
+    </div>
+
     <div class="container row d-flex justify-content-between">
-        <!-- 57 IPs - paginação - mais de 1mi de páginas -->
-        <?php foreach(array_chunk($allIPs, ceil(count($allIPs)/3)) as $row) { ?>
-            <div class="p-3 rounded bg-dark text-center" style="width: 33%;">
-                <?php foreach($row as $IP) { ?>
-                    <div style="color: #40F128"><?php echo $IP ?></div>
-                <?php } ?>
-            </div>
-        <?php } ?>
+        <?php 
+            if($allIPs == null) {
+                echo "<div class='bg-dark p-3 container rounded'>";
+                if(TITLE == 'IPs ocultos')
+                    echo "Nenhum IP foi ocultado até o momento";
+                else
+                    echo "IPs não encontrados!!";
+                echo "</div>";
+            } else { // slices all IPs in 3 arrays (row), each one with the same number of values
+                foreach(array_chunk($allIPs, ceil(count($allIPs)/3)) as $row) { ?>
+                    <div class="p-3 rounded bg-dark text-center" style="width: 33%;">
+                        <?php foreach($row as $IP) { ?>
+                            <div style="color: #40F128"><?php echo $IP ?></div>
+                        <?php } ?>
+                    </div>
+                <?php } 
+            } ?>
     </div>
 </div>
